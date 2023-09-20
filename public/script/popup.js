dc6e5226cd26527edf2907e18438d48eca892ebc
Emma Picard <emma.picard@ynov.com>
2023-09-20 21:18:27 +0200

    popup on the top finish, right part for the contact square finish but only the front, missing the back

--- public/script/popup.js
+++ public/script/popup.js
@@ -1,24 +1,55 @@
 //POPUP LINKEDIN
 let LinkedinImage = document.getElementById("linkedin");
-let closePopup = document.getElementById('closePopup');
+let closePopupLinkedin = document.getElementById('closePopupLinkedin');
 let popupLinkedin = document.getElementById('overlayLinkedin'); 
 
 LinkedinImage.addEventListener('click', function(event){
     event.preventDefault();
-    popupLinkedin.style.display = 'block';  
+    popupLinkedin.style.display = 'block'; 
+    document.body.style.overflow = 'hidden'; 
 });
 
 // Function to close the popup
 function closePopupFunc() {
     popupLinkedin.style.display = 'none';
+    document.body.style.overflow = '';
 }
 
 // Event listeners
-closePopup.addEventListener('click', closePopupFunc);
+closePopupLinkedin.addEventListener('click', closePopupFunc);
 
 // Close the popup when clicking outside of it
 window.addEventListener('click', function (event) {
     if (event.target === popupLinkedin) {
         closePopupFunc();
+    }
+});
+
+
+
+//POPUP GITHUB
+let GithubImage = document.getElementById("github");
+let closePopupGithub = document.getElementById('closePopupGithub');
+let popupGithub = document.getElementById('overlayGithub');
+
+GithubImage.addEventListener('click', function(event){
+    event.preventDefault();
+    popupGithub.style.display = 'block'; 
+    document.body.style.overflow = 'hidden'; 
+});
+
+// Function to close the popup
+function closePopupFuncGithub() {
+    popupGithub.style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+// Event listeners
+closePopupGithub.addEventListener('click', closePopupFuncGithub);
+
+// Close the popup when clicking outside of it
+window.addEventListener('click', function (event) {
+    if (event.target === popupGithub) {
+        closePopupFuncGithub();
     }
 });
